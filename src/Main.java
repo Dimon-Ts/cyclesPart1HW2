31cdf40cb5564bdda83e8ef0837a79e7f70bbee7
Dimon-Ts <tselishev.dimon@yandex.ru>
2022-09-22 18:03:41 +0300

    Циклы.Часть 1.Домашнее задание 2.

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задание 1
+        System.out.println(" Високосные годы ");
+        for( int i = 1904; i <= 2096; i = i + 4 ) {
+            System.out.println(i +" год является високосным ");
+        }
+        // Задание 2
+        System.out.println(" Последовательность чисел ");
+        for( int f = 7; f <= 98; f = f + 7 ) {
+            System.out.println("число " + f);
+        }
+        // Задание 3
+        System.out.println(" Последовательность чисел х 2");
+        for( int c = 1; c <= 512; c = c * 2 ) {
+            System.out.println("число " + c);
+        }
+
+
+    }
+}
